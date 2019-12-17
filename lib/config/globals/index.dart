a016e3f5596783afedfb7f45ef401d14434631d3
ivan eidel <ivan.eidelstein@gmail.com>
2019-12-17 11:39:40 -0300

    chore: Ajuste de detalles, comentarios, página de reglas, distancias, filtros

--- lib/config/globals/index.dart
+++ lib/config/globals/index.dart
@@ -2,13 +2,13 @@ import 'package:arrancando/config/globals/enums.dart';
 import 'package:flutter/material.dart';
 
 abstract class MyGlobals {
-  static const bool SHOW_DEV_LOGIN = true;
+  static const bool SHOW_DEV_LOGIN = false;
 
-  static const String SERVER_URL = "http://192.168.0.10:5000";
+  // static const String SERVER_URL = "http://192.168.0.10:5000";
   // static const String SERVER_URL = "http://192.168.0.11:5000";
   // static const String SERVER_URL = "http://192.168.43.138:5000";
   // static const String SERVER_URL = "http://192.168.1.3:5000";
-  // static const String SERVER_URL = "http://arrancando.herokuapp.com";
+  static const String SERVER_URL = "http://arrancando.herokuapp.com";
 
   static const GOOGLE_CLIENT_ID =
       "585563708448-5n5f3dg0ptbbm4p3eoh1l0c4u7fhrbrl.apps.googleusercontent.com";
