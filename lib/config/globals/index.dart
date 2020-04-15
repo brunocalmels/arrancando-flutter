7ae6845414ea0eb4f8e36967bb386c1111ae3d27
ivan eidel <ivan.eidelstein@gmail.com>
2020-04-15 13:43:38 -0300

    chore: Version

--- lib/config/globals/index.dart
+++ lib/config/globals/index.dart
@@ -26,7 +26,7 @@ abstract class MyGlobals {
   static const APPLE_REDIRECT_URI =
       "https://arrancando.herokuapp.com/apple-login";
 
-  static const APP_VERSION = "1.2.6+41";
+  static const APP_VERSION = "1.2.6+42";
 
   static const ICONOS_CATEGORIAS = {
     SectionType.home: Icons.select_all,
