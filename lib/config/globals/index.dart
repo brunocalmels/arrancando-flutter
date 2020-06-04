b7fb413fc2f2d7f67b23649740226de54a68ec0e
ivan eidel <ivan.eidelstein@gmail.com>
2020-06-04 11:15:14 -0300

    chore: Version + Fastlane

--- lib/config/globals/index.dart
+++ lib/config/globals/index.dart
@@ -4,7 +4,7 @@ import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 abstract class MyGlobals {
-  static const APP_VERSION = "2.0.7+51";
+  static const APP_VERSION = "2.0.8+52";
 
   static const bool SHOW_DEV_LOGIN = false;
   // static const bool SHOW_DEV_LOGIN = true;
